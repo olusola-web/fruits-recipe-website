378b01235bf704809f02d76a491d7c01ba6775f4
olusola-web <olusolakorode@gmail.com>
2023-10-02 17:55:22 +0100

    updated files

--- main.js
+++ main.js
@@ -36,3 +36,12 @@ const shadowHeader = () =>{
                        : header.classList.remove('shadow-header')
 }
 window.addEventListener('scroll', shadowHeader)
+
+// SCROLL UP
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    // when the scroll is higer than 350 viewport height, add the show-scroll class to be the a tag with the scrollup class
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+    : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
